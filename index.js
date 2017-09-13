4d2b2e5fd6c4ef2b44a482fc078bb1a3b056e5fa
Emma Konrad <emma.konrad92@gmail.com>
2017-09-13 23:39:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var katzDeli = [];
 function takeANumber(katzDeliLine, name) {
   var currentLine = [];
   for (let i = 1; i < line.length; i++) {
-    newPosition.push(`Welcome ${name[i]}. You are number ${line[i]} in line.`)
+    currentLine.push(`Welcome ${name[i]}. You are number ${line[i]} in line.`)
   }
   return newPosition
 }
